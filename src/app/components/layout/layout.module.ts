f56dd5bae4c169632c7637875e6f1f2407a6c6ab
Fabricio Blanco <fabriblanco.03@hotmail.com>
2023-10-03 15:46:05 -0300

    agrego rutas, componentes, modulos

---
+++ src/app/components/layout/layout.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { RouterLink, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ], 
+  exports: [
+    HeaderComponent,
+    NavbarComponent
+  ]
+})
+export class LayoutModule { }
